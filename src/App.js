f6c0888993520690af6eb56afaf14a321b83cd4b
JulianRengifo <jurenco1997@gmail.com>
2024-05-31 20:49:16 -0500

    Mostrar/Ocultar formulario
    Se maneja atraves de props y conditional rendering manejando el concepto ternario o uno mas corto denominado corto circuito

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-
+import { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header/Header.js'
 import Formulario from './componentes/Formulario/Formulario.js';
 import MiOrg from './componentes/MiOrg/index.js';
 
+//cuando trabajamos con estados tienen que estar dentro de un componente o una funcion
+
 function App() {
+
+  const [mostarFormulario, actualizarMosrar] = useState(true)
+  
+  // Ternario --> condicion ? seMuestra : noSemuestra  // funciona como un if else
+  // otro concepto seria corto circuito --> condicion && seMuestra --> {mostrarFormulario && <Formulario/>}
+
+  const cambiarMostrar = () => {
+    actualizarMosrar(!mostarFormulario)
+  }
   return (
     <div>
       <Header />
-      <Formulario/>
-      <MiOrg/>
+      {mostarFormulario ? <Formulario/> : <> </>}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -2,28 +2,30 @@
 import {useState} from "react"
 import "./MiOrg.css"
 
-const MiOrg = () => {
+const MiOrg = (props) => {
 
     //Estado - hooks
     //useState
     //useState()
 
-    console.log()
+    console.log(props)
 
     //const [nombreVariable,funcionActualiza] = useState(valorInicial)
     // useState("julian") crea una variable de estado (nombre) con un valor inicial de "julian" y una función (actualizarNombre) para actualizar ese estado.
-    const [nombre, actualizarNombre] = useState("julian") /*Permite para manejar el estado en un componente funcional */
-    const [mostrar, actualizarMosrar] = useState(true)
+    // const [nombre, actualizarNombre] = useState("julian") /*Permite para manejar el estado en un componente funcional */
+    
+    // const [mostrar, actualizarMosrar] = useState(true)
 
-    const manejarClick = () => {
-        console.log("Mostrar/Ocultar elemento" ,!mostrar) // se muestra el estado en true y se usa la negacion (!) para cambiar el estado
-        actualizarMosrar(!mostrar) // Actualiza el valor negado de mostar cambiando de true a false y viceversa
-    }
+    //const manejarClick = () => {
+    //    console.log("Mostrar/Ocultar elemento" ,!mostrar) // se muestra el estado en true y se usa la negacion (!) para cambiar el estado
+    //    actualizarMosrar(!mostrar) // Actualiza el valor negado de mostar cambiando de true a false y viceversa
+    //}
 
     return <section className="orgSection">
-        <h3 className="title">Mi organización {nombre}</h3>
-        <img src="/img/add.png" alt="add" onClick={manejarClick}/>
+        <h3 className="title">Mi organización</h3>
+        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar}/> {/* Cuando damos click en el boton activa el const cambiarMostrar */}
     </section>
+    
 }
 
 export default MiOrg;
