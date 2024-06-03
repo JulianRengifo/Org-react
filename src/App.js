f27cf1a1bfae1867948232c8b3ce8817170f350a
JulianRengifo <jurenco1997@gmail.com>
2024-06-02 20:19:45 -0500

    Componente equipo
    Se insertan los equipos atraves de props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './componentes/Header/Header.js'
 import Formulario from './componentes/Formulario/Formulario.js';
 import MiOrg from './componentes/MiOrg/index.js';
+import Equipo from './componentes/Equipo/index.js';
 
 //cuando trabajamos con estados tienen que estar dentro de un componente o una funcion
 
@@ -21,6 +22,13 @@ function App() {
       <Header />
       {mostarFormulario ? <Formulario/> : <> </>}
       <MiOrg cambiarMostrar={cambiarMostrar} />
+      <Equipo equipo="Programación"/>
+      <Equipo equipo="Front-End"/>
+      <Equipo equipo="Data Science"/>
+      <Equipo equipo="Devops"/>
+      <Equipo equipo= "UX y Diseño"/>
+      <Equipo equipo="Movil"/>
+      <Equipo equipo="Innovación y Gestion"/>
     </div>
   );
 }
