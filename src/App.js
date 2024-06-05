d5a60d849814e0f072feabfa3c8b89725137bd6a
JulianRengifo <jurenco1997@gmail.com>
2024-06-04 20:16:41 -0500

    Creando equipos
    Creamos un arreglo donde es los diferentes equipos en App.js
    
    Hacemos uso de la propiedad map para recorrer el array de objetos equipos
    
    Se usa style con el objeto para agregar el color usando props
    
    Se usa la destructuracion para no repetir props.datos

--- src/App.js
+++ src/App.js
@@ -17,18 +17,65 @@ function App() {
   const cambiarMostrar = () => {
     actualizarMosrar(!mostarFormulario)
   }
+
+  // Lista de equipos
+  const equipos = [
+
+    {
+    titulo: "Programacion",
+    colorPrimario: "#57C278",
+    colorSecundario: "#D9F7E9"
+    },
+
+    {
+      titulo: "Front-End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+
+    {
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    
+    {
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    
+    {
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+    
+]
+
+
   return (
     <div>
       <Header />
       {mostarFormulario ? <Formulario/> : <> </>}
       <MiOrg cambiarMostrar={cambiarMostrar} />
-      <Equipo equipo="Programación"/>
-      <Equipo equipo="Front-End"/>
-      <Equipo equipo="Data Science"/>
-      <Equipo equipo="Devops"/>
-      <Equipo equipo= "UX y Diseño"/>
-      <Equipo equipo="Movil"/>
-      <Equipo equipo="Innovación y Gestion"/>
+      {
+        /* este fragmento de código recorre un array de objetos llamado "equipos",
+        devuelve un conjunto de componentes "Equipo" con los datos de cada equipo como propiedades.*/
+        equipos.map ((equipo) => <Equipo datos={equipo} key={equipo.titulo}/>)
+      }
     </div>
   );
 }
--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -2,8 +2,22 @@
 import "./Equipo.css"
 
 const Equipo = (props) => {
-    return <section className="equipo">
-        <h3>{props.equipo}</h3>
+
+    // Destructuración
+    // Se usa la destructuracion para no repetir props.datos
+
+    const {colorPrimario, colorSecundario, titulo} = props.datos
+
+    const colorFondo = {
+        /*establece el color de fondo de algún elemento en función del valor de 
+        colorSecundario dentro de los datos proporcionados a través de props.*/
+        backgroundColor: colorSecundario
+    }
+
+    const colorCards = {borderColor: colorPrimario}
+
+    return <section className="equipo" style={colorFondo}>
+        <h3 style={colorCards}>{titulo}</h3>
         <div className="colaboradores"></div>
     </section>
 }
